10ab50bc5f3a5e0b227d0d607943d8e921eebb9f
Docker Brat <lymondgl@student.21-school.ru>
2022-11-30 18:52:12 +0300

    solutions for day 01

--- SQL_beginner._Day01-0/src/ex00/day01_ex00.sql
+++ SQL_beginner._Day01-0/src/ex00/day01_ex00.sql
@@ -1,2 +1,5 @@
-SELECT id AS object_id, name AS object_name FROM person
-UNION ALL  SELECT id, pizza_name FROM menu ORDER BY object_name;
+SELECT * FROM
+	(SELECT id AS object_id, pizza_name AS object_name FROM menu
+	UNION
+	SELECT id AS object_id, name AS object_name FROM person) t1
+ORDER BY object_id, object_name;
--- SQL_beginner._Day01-0/src/ex05/day01_ex05.sql
+++ SQL_beginner._Day01-0/src/ex05/day01_ex05.sql
@@ -1,4 +1,2 @@
-SELECT person.id AS "person.id" , person.name AS "person.name", age,
-       gender, address, pizzeria.id AS "pizzeria.id", pizzeria.name AS "pizzeria.name",
-       rating
-FROM pizzeria CROSS JOIN person ORDER BY  "person.id", "pizzeria.id";
+SELECT * FROM person, pizzeria
+ORDER BY person.id, pizzeria.id;
