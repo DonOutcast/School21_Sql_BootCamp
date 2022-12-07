8a868b716ad4a620152fd6e497ebfc180e1da272
Docker Brat <lymondgl@student.21-school.ru>
2022-12-07 16:45:14 +0300

    some fix

--- SQL_beginner._Day06-0/src/ex02/day06_ex02.sql
+++ SQL_beginner._Day06-0/src/ex02/day06_ex02.sql
@@ -3,8 +3,12 @@ SELECT person.name                                            AS name,
        price,
        (menu.price / 100 * (100 - person_discounts.discount)) AS discount_price,
        pizzeria.name                                          AS pizzeria_name
-FROM person_discounts
-         LEFT JOIN person ON person.id = person_discounts.person_id
-         LEFT JOIN menu ON menu.pizzeria_id = person_discounts.pizzeria_id
-         LEFT JOIN pizzeria ON pizzeria.id = menu.pizzeria_id
+FROM person
+JOIN person_order
+ON person_order.person_id = person.id
+JOIN menu
+ON menu.id = person_order.menu_id
+JOIN person_discounts
+ON person_discounts.person_id = person_order.person_id
+JOIN pizzeria ON menu.pizzeria_id = pizzeria.id
 ORDER BY 1, 2;
